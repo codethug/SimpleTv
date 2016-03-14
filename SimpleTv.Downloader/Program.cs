20850479ba9e2b22a4c738dd9606c781b50969da
codethug <tlarbo@gmail.com>
2016-03-14 11:26:54 -0400

    Moving TODO comments to github issues

--- SimpleTv.Downloader/Program.cs
+++ SimpleTv.Downloader/Program.cs
@@ -8,18 +8,6 @@ namespace SimpleTv.Downloader
     {
         static void Main(string[] args)
         {
-            // Todo: Refactor and add tests
-            // Todo: Intelligently figure out what files remain to be downloaded (don't redownload if we already have it)
-            //      Perhaps compare file size locally with file size on server for each file
-            // Todo: After building replacement dictionary, filter out each term to avoid illegal characters
-            // Todo: Verify that progress bar / percent downloading works, perhaps add 'time remaining'
-            // Todo: Remove Username/Password/BaseDownloadFolder from here
-            // Todo: Checkin to GitHub
-            // Todo: Remove all Console.WriteLine events from SDK, add Events that are consumed by Program
-            // Todo: Resume downloads that are interrupted (if supported by server)
-            // Todo: Setup Plex to Auto-Scan
-            // Todo: Setup Download as Scheduled Task on Plex box from Simple.Tv box
-
             var username = "simpletvusername";
             var password = "simpletvpassword";
             var baseDownloadFolder = "D:\\TV";
